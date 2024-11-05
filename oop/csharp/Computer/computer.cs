a5e2d0b44eac6fd9ed079be556dcdcd757d0319a
marisolperezr <marisolperezr@correo.ugr.es>
2024-11-05 17:09:19 +0100

    No repeated random numbers

--- oop/csharp/Computer/computer.cs
+++ oop/csharp/Computer/computer.cs
@@ -1,16 +1,9 @@
-/******************************************************************************
-
-Maria Soledad Perez Ruiz
-
-*******************************************************************************/
-
-
 using System;
+using System.Collections.Generic;                   // Included this for List<int> 
 
-//namespace OOP_in_Csharp{   //cambiar el nombre al que queramos
 public class Computer
 {
-    // Atributes
+    // Attributes
     private string biosName; 
     private string ipAddress; 
 
@@ -25,11 +18,11 @@ public class Computer
         }
     }
 
-    public string IpAddress{
-        get{
+    public string IpAddress {
+        get {
             return this.ipAddress; 
         }
-        set{
+        set {
             this.ipAddress = value; 
         }
     }
@@ -39,42 +32,53 @@ public class Computer
     {
         BiosName = biosName;
         IpAddress = ipAddress;
-        counter ++;
+        counter++;
     }
 
     // Default constructor
     public Computer() {
         counter++;
     }
-    public static int getCompNum(){
+
+    public static int getCompNum() {
         return counter;
     } 
-    public void ShowInfo()
-    {
+    public void ShowInfo() {
         Console.WriteLine($"BiosName: {BiosName}, IpAddress: {IpAddress}");
     }
 }
 
 public class Program
 {
-    static Random random = new Random(); // Move Random out of the methor to avoid creating multiple instances
+    static Random random = new Random();            // Move Random out of the method to avoid creating multiple instances
 
-    static string getNum()              // Static method to generate random numbers
-    {
-        int num = random.Next(1, 255);  // Generates a number between min (inclusive) and  max (exclusive)
-        return num.ToString();
+    // How to be sure no number is ever repeated
+
+    // Create a list of already used numbers
+    static List<int> UsedNumbers = new List<int>();
+
+    static string getNum() {                        // Static method to generate random numbers
+        if (UsedNumbers.Count == 0) {
+            for (int i = 1; i <= 254; i++) {        // At first, mi list is empty so I fill it with all the values between 0 and 255
+                UsedNumbers.Add(i);
+            }
+        }
+
+        int index = random.Next(UsedNumbers.Count); // UsedNumbers count is the total number of numbers I have in my list UsedNumbers. So I generate a random number with random.next between 0 and 254,  in this case
+        int num = UsedNumbers[index];               // When I have the number, for example, if it is 3 we would have int num=UsedNumbers[3] and the number would be the one on the third position
+        UsedNumbers.RemoveAt(index);                // I erase the selected number so itś no longer available
+
+        return num.ToString();                      // Returns the selected number as a string
     }
 
     public static void Main()  
     {
         Computer[] net = new Computer[4];
-        for (int i = 0; i < net.Length; i++)
-        {
+        for (int i = 0; i < net.Length; i++) {
             net[i] = new Computer("comp" + i.ToString(), "10.0." + getNum() + "." + getNum());
         }
 
-        for (int i = 0; i < net.Length; i++)
-        {
+        for (int i = 0; i < net.Length; i++) {
             Console.WriteLine("{0} --- {1}", net[i].BiosName, net[i].IpAddress);
         }
 
@@ -92,10 +96,24 @@ public class Program
         Console.WriteLine(comp01.BiosName + " - " + comp01.IpAddress);
         Console.WriteLine("We have {0} computers!!", Computer.getCompNum());
 
-        // Show ShowInfo
         
+
+        // Show ShowInfo
+
         computer1.ShowInfo();
         computer2.ShowInfo();
         comp01.ShowInfo();
     }
 }
+
+
+// teacher's version
+// static void Main()
+// int comNum = 50;
+// string[] ipadr = new String [compNum];
+//for (int i=0; i< compNum ; i++){
+// ipadr[i] = "10.0.0.0" + (i+1).ToString();
+//Console.WriteLine(ipadr[i]);
+//}
+//}
+//}

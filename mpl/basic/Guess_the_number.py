d4bea4df9c9e4e7b27fdb40fa88cbb9d64e7a473
marisolperezr <marisolperezr@correo.ugr.es>
2024-11-10 13:15:30 +0100

    Guess_the_number_2

--- mpl/basic/Guess_the_number.py
+++ mpl/basic/Guess_the_number.py
@@ -1,12 +1,11 @@
 import random
 
-total_tries = 0;
+total_tries = 0
 secret_num=10
 
 while True:
     a = int(input("Enter the number from 1 to 100: "))
-    total_tries+=1;
-    if a == secret_num:
+    total_tries+=1
         print("You guessed!")
         break
     elif a > secret_num:
